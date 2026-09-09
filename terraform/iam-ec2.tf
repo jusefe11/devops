2c78f483c465c47083343f5632c6625631867795
Juan Sebastián Ferrer Bustos <jusefe11@hotmail.com>
2026-09-09 17:03:07 -0500

    feat: habilitar SSM en instancia EC2

--- terraform/iam-ec2.tf
+++ terraform/iam-ec2.tf
@@ -25,3 +25,8 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-ec2-profile"
   role = aws_iam_role.ec2_ecr_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
