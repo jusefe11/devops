4864ccef6de55d969c57b26e242d9e2bac00ff1d
Juan Sebastián Ferrer Bustos <jusefe11@hotmail.com>
2026-09-09 12:56:05 -0500

    feat: agregar SSH key pair para EC2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -21,6 +21,20 @@ data "aws_ami" "ubuntu" {
 
 
 # ============================================================
+# SSH KEY PAIR
+# ============================================================
+
+resource "aws_key_pair" "devops" {
+  key_name   = "${var.project_name}-key"
+  public_key = file("${path.module}/hola-juan-devops.pub")
+
+  tags = {
+    Name = "${var.project_name}-key"
+  }
+}
+
+
+# ============================================================
 # INSTANCIA EC2
 # ============================================================
 
@@ -28,10 +42,15 @@ resource "aws_instance" "devops" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
+  # Llave SSH para conectarnos desde nuestro PC
+  key_name = aws_key_pair.devops.key_name
+
+  # Configuracion de red
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   associate_public_ip_address = true
 
+  # Disco principal
   root_block_device {
     volume_type = "gp3"
     volume_size = 20
