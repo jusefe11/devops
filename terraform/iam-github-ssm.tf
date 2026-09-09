d22e74a25b1abf48daf689d12de8a5210e85515f
Juan Sebastián Ferrer Bustos <jusefe11@hotmail.com>
2026-09-09 17:11:26 -0500

    feat: permitir despliegues SSM desde GitHub Actions

---
+++ terraform/iam-github-ssm.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_role_policy" "github_ssm_deploy" {
+  name = "hola-juan-devops-github-ssm-deploy"
+  role = "devops-juan"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ssm:SendCommand"
+        ]
+
+        Resource = [
+          aws_instance.devops.arn,
+          "arn:aws:ssm:${var.aws_region}::document/AWS-RunShellScript"
+        ]
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ssm:GetCommandInvocation",
+          "ssm:ListCommandInvocations"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
