febae58a1d5db3c80e1438ab78e08ed24197d86a
Juan Sebastián Ferrer Bustos <jusefe11@hotmail.com>
2026-09-09 23:26:27 -0500

    feat: automatizar bootstrap EC2 y acceso frontend

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,6 +8,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name = "name"
+
     values = [
       "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
     ]
@@ -42,25 +43,62 @@ resource "aws_instance" "devops" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
-  # Llave SSH para conectarnos desde nuestro PC
+  # ==========================================================
+  # BOOTSTRAP AUTOMATICO
+  # ==========================================================
+  # Ejecuta user-data.sh cuando se crea una EC2 nueva.
+  # Instala Docker, kubectl, Minikube, AWS CLI y Nginx.
+  user_data = file("${path.module}/user-data.sh")
+
+
+  # ==========================================================
+  # SSH
+  # ==========================================================
+
   key_name = aws_key_pair.devops.key_name
 
-  # IAM Instance Profile
-  # Permite que la EC2 asuma el IAM Role configurado
-  # para acceder a Amazon ECR sin Access Keys
+
+  # ==========================================================
+  # IAM INSTANCE PROFILE
+  # ==========================================================
+  # Permite que la EC2 obtenga credenciales temporales
+  # mediante IAM Role.
+  #
+  # Actualmente permite:
+  # - Descargar imagenes desde Amazon ECR
+  # - Comunicarse con AWS Systems Manager
+  # ==========================================================
+
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-  # Configuracion de red
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.ec2.id]
+
+  # ==========================================================
+  # RED
+  # ==========================================================
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2.id
+  ]
+
   associate_public_ip_address = true
 
-  # Disco principal
+
+  # ==========================================================
+  # DISCO PRINCIPAL
+  # ==========================================================
+
   root_block_device {
     volume_type = "gp3"
     volume_size = 20
     encrypted   = true
   }
+
+
+  # ==========================================================
+  # TAGS
+  # ==========================================================
 
   tags = {
     Name = "${var.project_name}-ec2"
